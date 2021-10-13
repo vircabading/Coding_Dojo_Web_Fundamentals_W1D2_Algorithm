6117912d35dd841596b5dc4054d243c2fafb624f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-13 05:55:08 -0700

    Lesson 3

--- Algorithms.js
+++ Algorithms.js
@@ -3,17 +3,17 @@
 // W1D2
 /////////////////////////////////////////////////
 
-/////////////////////////////////////////////////
-// Lesson 1
-// var a = 25;
-// a = a - 13;
-// console.log(a/2);
+console.log("/////////////////////////////////////////////////");
+console.log("Lesson 1"); 
+var a = 25;
+a = a - 13;
+console.log(a/2);
     
-// a = "hello";
-// console.log(a + " hello");
+a = "hello";
+console.log(a + " hello");
 
-//////////////////////////////////////////////////
-// Lesson 2
+console.log("/////////////////////////////////////////////////");
+console.log("Lesson 2");
 for(var i=0; i<10; i++) {
     console.log(i);
     i = i + 3; 
@@ -21,3 +21,19 @@ for(var i=0; i<10; i++) {
     
 console.log("outside of the loop " + i);
 
+console.log("/////////////////////////////////////////////////");
+console.log("Lesson 3");
+function getTotal(arrayOfNumbers) {
+    
+    var sum = arrayOfNumbers[0];
+      
+    for(var i=0; i<arrayOfNumbers.length; i++) {
+      sum += arrayOfNumbers[i];
+      console.log("the current sum is: " + sum); 
+    }
+      
+    console.log("the total is: " + sum);
+      
+  }
+      
+getTotal([1, 3, 5]);  
