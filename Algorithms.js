8a82d296c3f1541becad3762514675f59968c840
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-13 10:16:08 -0700

    initial

--- Algorithms.js
+++ Algorithms.js
@@ -24,7 +24,7 @@ console.log("outside of the loop " + i);
 console.log("/////////////////////////////////////////////////");
 console.log("Lesson 3");
 function getTotal(arrayOfNumbers) {
-    
+
     var sum = arrayOfNumbers[0];
       
     for(var i=0; i<arrayOfNumbers.length; i++) {
@@ -35,5 +35,5 @@ function getTotal(arrayOfNumbers) {
     console.log("the total is: " + sum);
       
   }
-      
+
 getTotal([1, 3, 5]);  
