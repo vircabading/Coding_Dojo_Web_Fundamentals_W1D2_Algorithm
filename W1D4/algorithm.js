0ba13ac7c7d57b23bdd952761558d1e36e703310
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-17 19:10:55 -0700

    add code flow functions algorithm

--- W1D4/algorithm.js
+++ W1D4/algorithm.js
@@ -7,3 +7,17 @@
 import { print_desc } from "./module.js";
 import { print_ln } from "./module.js";
 
+//// CODE FLOW - FUNCTIONS //////////////////////
+print_desc("Code Flow - Funcitons");
+
+var x = 5;
+    
+function setX(newValue) {
+    x = newValue; 
+}
+    
+console.log(x);
+setX(15);
+console.log(x);
+print_ln();
+
