2ffcaaea1207a8a5878a36b79def1b3cd116a6ce
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-17 19:34:11 -0700

    add while loop algorithm

--- W1D5/algorithm.js
+++ W1D5/algorithm.js
@@ -18,6 +18,7 @@ console.log(fruit2 + " and " + fruit1);
 print_ln();
 
 //// HOW TO SWAP VARIABLES version 2 ////////////
+print_desc("How to swap variable Version 2")
 
 var fruit1 = "apples";
 var fruit2 = "oranges";
@@ -31,4 +32,14 @@ print_ln();
 
 //// WHILE LOOPS ////////////////////////////////
 print_desc("While Loops");
+
+var start = 0;
+var end = 12;
+    
+while(start < end) {
+    console.log("start: " + start + ", end: " + end);
+    start += 2;
+    end -= 2;
+}
+print_ln();
 
