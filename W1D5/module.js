326b9cd2002b0e951671a2baf3af60590a5ae972
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-17 19:21:22 -0700

    Initialize

---
+++ W1D5/module.js
@@ -0,0 +1,9 @@
+export function print_desc(description) {                     // Function to print descriptions onto the console
+    console.log ("//////////////////////////////////////////////////")
+    console.log(description);
+    console.log("");
+}
+
+export function print_ln() {                                   // Function to print an empty line 
+    console.log("");
+}
