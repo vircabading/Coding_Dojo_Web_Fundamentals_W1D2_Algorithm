06927d4674ca8044b06eb18d049a599331ec1cda
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-17 19:30:08 -0700

    add how to swap variables algorithm

--- W1D5/algorithm.js
+++ W1D5/algorithm.js
@@ -4,3 +4,27 @@
 // Created: October 17, 2021
 /////////////////////////////////////////////////
 
+import { print_desc } from "./module.js";
+import { print_ln } from "./module.js";
+
+//// HOW TO SWAP VARIABLES //////////////////////
+print_desc("How to swap variables");
+var fruit1 = "apples";
+var fruit2 = "oranges";
+    
+fruit2 = fruit1;
+    
+console.log(fruit2 + " and " + fruit1);
+print_ln();
+
+//// HOW TO SWAP VARIABLES version 2 ////////////
+
+var fruit1 = "apples";
+var fruit2 = "oranges";
+    
+var temp = fruit1; // temp is a common name for this
+fruit1 = fruit2;
+fruit2 = temp;
+    
+console.log(fruit2 + " and " + fruit1);
+print_ln();
