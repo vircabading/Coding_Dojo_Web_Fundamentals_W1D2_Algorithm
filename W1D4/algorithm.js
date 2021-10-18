97c5f5b9cd21d3481f642f521677f28f99b2c45f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-17 19:17:24 -0700

    add Is the array a palindrome algorithm

--- W1D4/algorithm.js
+++ W1D4/algorithm.js
@@ -37,3 +37,23 @@ console.log(result);
 console.log(x);
 
 print_ln();
+
+//// CODE FLOW - IS THE ARRAY A PLALINDROME /////
+print_desc("Code Flow - Is the array a palindrome");
+
+function isPal(arr) {
+    for(var left=0; left<arr.length/2; left++) {
+        var right = arr.length-1-left;
+        if(arr[left] != arr[right]) {
+            return "Not a pal-indrome!";
+        }
+    }
+    return "Pal-indrome!";
+}
+    
+var result1 = isPal( [1, 1, 2, 2, 1] );
+console.log(result1);
+    
+var result2 = isPal( [3, 2, 1, 1, 2, 3] );
+console.log(result2);
+
