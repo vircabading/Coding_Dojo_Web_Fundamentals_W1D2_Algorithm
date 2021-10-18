723db02137bbf6e345a3502c6d01bf570b675a22
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-17 19:14:42 -0700

    Add The Return ot return algorithm

--- W1D4/algorithm.js
+++ W1D4/algorithm.js
@@ -21,3 +21,19 @@ setX(15);
 console.log(x);
 print_ln();
 
+//// THE RETURN OF RETURN ///////////////////////
+print_desc("The Return of return");
+
+var x = 5;
+    
+function addToX(amount) {
+    return x + amount;
+    console.log("hello there");
+}
+    
+console.log(x);
+var result = addToX(-10);
+console.log(result);
+console.log(x);
+
+print_ln();
